4ada74b0721d9d7daf2d460c3a796b6b7f9e204f
seok jin JU <74058894+fast00@users.noreply.github.com>
2024-06-23 11:57:33 +0900

    Update train.py

--- train_server/src/train.py
+++ train_server/src/train.py
@@ -6,24 +6,47 @@ from data_gen import create_folder
 import numpy as np
 import os
 
-metric = load_metric("seqeval")
-return_entity_level_metrics = False
+class TimePredictionToFileCallback(TrainerCallback):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def on_train_begin(self, args, state, control, **kwargs):
+        self.start_time = time.time()
+
+    def on_step_end(self, args, state, control, **kwargs):
+        current_time = time.time()
+        step_duration = current_time - (getattr(self, 'last_step_time', current_time))
+        self.last_step_time = current_time
+
+        remaining_steps = state.max_steps - state.global_step
+        estimated_time_left = remaining_steps * step_duration
+        estimated_time_left = timedelta(seconds=int(estimated_time_left))
+        completion_percentage = round((state.global_step / state.max_steps) * 100, 2)
+        with open(self.file_path, 'w') as f:
+            f.write(f"{{ 'percentage':'{completion_percentage}%', 'remain' :'{estimated_time_left}'}}")
 
 
 class TrainModel:
-    def __init__(self,processor, train_dataset, eval_dataset, label_list):
+    def __init__(self,processor, train_dataset, eval_dataset, label_list,s3_client, bucket_name, user_id):
+        self.s3_client = s3_client
+        self.bucket_name = bucket_name
+        self.user_id = user_id
         self.label_list = label_list
         id2label = {k: v for k, v in enumerate(label_list)}
         label2id = {v: k for k, v in enumerate(label_list)}
         self.train_dataset = train_dataset
         self.eval_dataset = eval_dataset
         self.processor = processor
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = LayoutLMv3ForTokenClassification.from_pretrained("microsoft/layoutlmv3-base",
                                                          id2label=id2label,
                                                          label2id=label2id,
-                                                         num_labels=len(label2id))
+                                                         num_labels=len(label2id)).to(self.device)
+        self.metric = load_metric("seqeval")
+        self.return_entity_level_metrics = False
 
     def compute_metrics(self, p):
+
         predictions, labels = p
         predictions = np.argmax(predictions, axis=2)
 
@@ -37,8 +60,8 @@ class TrainModel:
             for prediction, label in zip(predictions, labels)
         ]
 
-        results = metric.compute(predictions=true_predictions, references=true_labels)
-        if return_entity_level_metrics:
+        results = self.metric.compute(predictions=true_predictions, references=true_labels)
+        if self.return_entity_level_metrics:
             # Unpack nested dictionaries
             final_results = {}
             for key, value in results.items():
@@ -56,12 +79,16 @@ class TrainModel:
                 "accuracy": results["overall_accuracy"],
             }
 
-    def train(self,tmp_dir):
-        tmp_dir = create_folder(tmp_dir)
+    def save_model(self, target_folder):
+          for file_name in os.listdir(target_folder):
+            with open(os.path.join(os.path.join(target_folder, file_name)), 'rb') as f:
+              self.s3_client.put_object(Bucket=self.bucket_name, Key=(f'{self.user_id}/model/{file_name}'), Body=f)
+
+    def train(self,tmp_dir, log_path):
         model_path = os.path.join(tmp_dir, "model")
         training_args = TrainingArguments(output_dir=tmp_dir,
                                           overwrite_output_dir='True',
-                                          num_train_epochs=5,
+                                          num_train_epochs=10,
                                           per_device_train_batch_size=1,
                                           per_device_eval_batch_size=1,
                                           learning_rate=1e-5,
@@ -83,7 +110,10 @@ class TrainModel:
             tokenizer=self.processor,
             data_collator=default_data_collator,
             compute_metrics=self.compute_metrics,
+            # train_dataloader=train_dataloader, 
+            # eval_dataloader=eval_dataloader
         )
-
+        trainer.add_callback(TimePredictionToFileCallback(log_path))
         trainer.train()
         trainer.save_model(model_path)
+        self.save_model(model_path)
